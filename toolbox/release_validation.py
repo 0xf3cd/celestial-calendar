6d077ac848c3daa50a8a126e9feae20ad4fbda4e
0xf3cd <nq.maigre@gmail.com>
2026-08-17 10:50:49 -0700

    fix(release): add the full GPL file header

--- toolbox/release_validation.py
+++ toolbox/release_validation.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python3
 #
-# Validate release archive payloads without extracting or modifying them.
+# CelestialCalendar:
+#   A C++23-style library that performs astronomical calculations and date conversions among various calendars,
+#   including Gregorian, Lunar, and Chinese Ganzhi calendars.
 #
-#########################################################################################
+# Copyright (C) 2026 Ningqi Wang (0xf3cd)
+# Email: nq.maigre@gmail.com
+# Repo : https://github.com/0xf3cd/celestial-calendar
 #
-# CelestialCalendar Automation:
-#   Python automation scripts for building and testing the CelestialCalendar C++ project.
+# This project is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-# Author : Ningqi Wang (0xf3cd)
-# Email  : nq.maigre@gmail.com
-# Repo   : https://github.com/0xf3cd/celestial-calendar
-# License: GNU General Public License v3.0
+# This project is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
 #
-# This software is distributed without any warranty.
-# See <https://www.gnu.org/licenses/> for more details.
+# You should have received a copy of the GNU General Public License
+# along with this project. If not, see <https://www.gnu.org/licenses/>.
 
 import hashlib
 import json
