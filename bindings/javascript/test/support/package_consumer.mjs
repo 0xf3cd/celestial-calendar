9ac955f234a1ec2b25cf37fc47616d7053fb2779
0xf3cd <nq.maigre@gmail.com>
2026-08-20 11:28:31 -0700

    style(javascript): restore the consumer file header

--- bindings/javascript/test/support/package_consumer.mjs
+++ bindings/javascript/test/support/package_consumer.mjs
@@ -1,6 +1,7 @@
 /*
  * CelestialCalendar:
- *   A C++23-style library that performs astronomical calculations and date conversions among various calendars.
+ *   A C++23-style library that performs astronomical calculations and date conversions among various calendars,
+ *   including Gregorian, Lunar, and Chinese Ganzhi calendars.
  *
  * Copyright (C) 2026 Ningqi Wang (0xf3cd)
  * Email: nq.maigre@gmail.com
